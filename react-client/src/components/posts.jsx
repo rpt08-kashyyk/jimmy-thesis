9167f6ca2e4c996f1fc5020fc7c5426719768c49
Jimmy Phu <mr.jphu@gmail.com>
2018-10-25 11:37:57 -0700

    profile pic added

--- react-client/src/components/posts.jsx
+++ react-client/src/components/posts.jsx
@@ -4,7 +4,10 @@ const Posts = (props) => (
   <div>
     {props.data.map((post) => 
     	<div key={post._id}>{post.reviews.map((review, i) =>
-    		<div key={i}>{review.comment}<hr></hr></div>
+    		<div key={i}>
+					<img src={review.userImage} height="48" width="48"/><br></br>
+	    		{review.comment}<hr></hr>
+    		</div>
     	)}</div>
     )}
   </div>
