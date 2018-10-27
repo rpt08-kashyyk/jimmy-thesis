0535556318e8b9e2d4b170f19352fa917c40bd05
Jimmy Phu <mr.jphu@gmail.com>
2018-10-26 21:55:50 -0700

    search bar created, functions to be written

--- react-client/src/components/ratings.jsx
+++ react-client/src/components/ratings.jsx
@@ -4,8 +4,14 @@ const Ratings = (props) => (
   <div>
     {props.data.map((post) =>
     	<div key={post._id}>
-	    	<h2>{`${post.totalReviews.total} Reviews `}</h2>
-	    	<h2>{props.star(post.totalReviews.average)}</h2>
+	    	<h2>{`${post.totalReviews.total} Reviews `}{props.star(post.totalReviews.average)}</h2>
+ 			<form>
+       <input
+         placeholder="Search reviews"
+       />
+       <p></p>
+     </form>
+	    	<hr></hr>
 	    	{`Accuracy `}{props.star(post.accuracy)}{` Location `}{props.star(post.location)}<br></br>
 	    	{`Communication `}{props.star(post.communication)}{` Check-in `}{props.star(post.checkin)}<br></br>
 	    	{`Cleanliness `}{props.star(post.cleanliness)}{` Value `}{props.star(post.value)}<br></br>
