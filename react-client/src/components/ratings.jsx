6deaeb8d077f66d2e51dcd30ac9283c5a6f578a8
Jimmy Phu <mr.jphu@gmail.com>
2018-10-30 17:38:45 -0700

    spacing corrected for ratings component

--- react-client/src/components/ratings.jsx
+++ react-client/src/components/ratings.jsx
@@ -14,17 +14,14 @@ const Ratings = (props) => (
 		     	</form>
 		    </div>
 		    	<hr></hr>
-		    	<br></br>
 		    <div className="middle-container">
 		    	{'Accuracy'}{props.star(post.accuracy)}
 		    	{'Location'}{props.star(post.location)}
 				</div>
-		    	<br></br>
 		    <div className="middle-container">
 		    	{'Communication'}{props.star(post.communication)}
 		    	{'Check-in'}{props.star(post.checkin)}
 		    </div>
-		    	<br></br>
 		    <div className="middle-container">
 		    	{'Cleanliness'}{props.star(post.cleanliness)}
 		    	{'Value'}{props.star(post.value)}
